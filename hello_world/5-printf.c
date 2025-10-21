81668bc7fc161c44ad377257f143f286159c27f5
yacine337 <yacinecho33@gmail.com>
2025-10-21 08:47:35 +0000

    phrase qui utilise printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - point d'entree du programme 
+ *
+ * Description: affiche une phrase printf
+ *
+ * Return: 0 si le programme s'execute correctement
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
