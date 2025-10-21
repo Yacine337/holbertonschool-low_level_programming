edafaf0aa363b040c5ceed4a865aa7a83070953d
yacine337 <yacinecho33@gmail.com>
2025-10-21 07:52:03 +0000

    programme C qui affiche texte

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main(void)
+/**
+ * main - point d'entree du programme
+ *
+ * Description: affiche la phrase demandee
+ *
+ *Retunrn: toujours 0 succes
+ */
+Int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0); /* Le programme se termine avec la valeur 0 */
