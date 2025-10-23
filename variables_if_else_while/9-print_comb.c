139d3839ad3feb15b0c1172ee794c6cc261137b8
yacine337 <yacinecho33@gmail.com>
2025-10-23 09:17:36 +0000

    les nombres doivent etre separer par ,, suivi d'un espace

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - affiche toutes les combinaisons possible
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
