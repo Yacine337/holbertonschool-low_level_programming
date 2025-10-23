ba330ef55eb55a554fdbef592c13f569491248c3
yacine337 <yacinecho33@gmail.com>
2025-10-23 08:47:25 +0000

    afficher minuscule a l'envers

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <unh>
+/**
+ * main - Print the lowercase alphabet in reverse, followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
